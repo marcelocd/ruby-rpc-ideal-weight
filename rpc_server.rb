850632b22db5bb29c724a60c9a41618766b5004e
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-03-06 15:57:21 -0300

    Create server

---
+++ rpc_server.rb
@@ -0,0 +1,20 @@
+require 'xmlrpc/server'
+
+server = XMLRPC::Server.new(3000)
+
+server.add_handler('ideal_weight') do |gender, height|
+  case gender
+  when 'f'
+    ((62.1 * height) - 44.7).round(3)
+  when 'm'
+    ((72.7 * height) - 58).round(3)
+  else
+    raise XMLRPC::FaultException.new(1, 'Invalid gender or height')
+  end
+end
+
+server.set_default_handler do |name, *args|
+  raise XMLRPC::FaultException.new(-99, "Method #{name} or wrong number of parameters!")
+end
+
+server.serve
